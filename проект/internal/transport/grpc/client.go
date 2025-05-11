516ee2fc6b125d46668e4e846bc05157e03623e2
opr1234 <karas4ik601@gmail.com>
2025-05-12 00:55:30 +0400

    Update client.go

--- проект/internal/transport/grpc/client.go
+++ проект/internal/transport/grpc/client.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"time"
 
-	pb "github.com/yourusername/calculator/proto"
+	"github.com/opr1234/calculator/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
